12c7b687b09072ff7943b9ffc5b461d78faf04e4
Tolik <t1005828@gmail.com>
2024-07-01 00:49:37 +0300

    implemented splash screen

---
+++ app/src/main/java/com/mvdasker/geeks_pro_mvd/presenter/ui/fragments/auth/AuthorizationFragment.kt
@@ -0,0 +1,18 @@
+package com.mvdasker.geeks_pro_mvd.presenter.ui.fragments.auth
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.mvdasker.geeks_pro_mvd.R
+import com.mvdasker.geeks_pro_mvd.databinding.FragmentAuthorizationBinding
+import com.mvdasker.geeks_pro_mvd.utils.ext.viewBinding
+
+class AuthorizationFragment : Fragment(R.layout.fragment_authorization) {
+
+    private val binding by viewBinding(FragmentAuthorizationBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
