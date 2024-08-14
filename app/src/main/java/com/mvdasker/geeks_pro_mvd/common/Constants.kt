8d0539339073b612be84c9b620a7f4b3bff28e73
Tolik <t1005828@gmail.com>
2024-08-14 16:35:34 +0300

    fixed bag

--- app/src/main/java/com/mvdasker/geeks_pro_mvd/common/Constants.kt
+++ app/src/main/java/com/mvdasker/geeks_pro_mvd/common/Constants.kt
@@ -1,7 +1,7 @@
 package com.mvdasker.geeks_pro_mvd.common
 
 object Constants {
-    const val BASE_URL = "http://209.38.228.54:83/admin/"
+    const val BASE_URL = "http://209.38.228.54:83/api/v1/"
     const val MANAGEMENT_END_POINT = "docs/governance/kr/"
     const val LAW_END_POINT = "docs/law/"
     const val CHARTERS_END_POINT = "docs/charter/"
