61ec82399157cb564b4c684b1b34e2edbe7e992c
Tolik <t1005828@gmail.com>
2024-08-20 15:56:56 +0300

    fixed code

--- app/src/main/java/com/mvdasker/geeks_pro_mvd/presentation/ui/fragments/home/news/NewsFragment.kt
+++ app/src/main/java/com/mvdasker/geeks_pro_mvd/presentation/ui/fragments/home/news/NewsFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -96,6 +97,7 @@ class NewsFragment : Fragment() {
 
                 is Messages.NetworkIsDisconnected -> {
                     noInternetSnackbar()
+                    binding.cardView.isVisible = false
                 }
             }
             viewModel.clearMessage()
