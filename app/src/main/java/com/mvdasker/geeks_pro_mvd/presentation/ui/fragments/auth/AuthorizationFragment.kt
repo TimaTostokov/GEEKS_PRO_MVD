b1958b66c1d1e608ec73f194e78de1874d4990ed
alexandrsulatskov <sulatskovalexandr97@gmail.com>
2024-09-11 00:43:38 +0400

    laws task fixed code

--- app/src/main/java/com/mvdasker/geeks_pro_mvd/presentation/ui/fragments/auth/AuthorizationFragment.kt
+++ app/src/main/java/com/mvdasker/geeks_pro_mvd/presentation/ui/fragments/auth/AuthorizationFragment.kt
@@ -59,8 +59,6 @@ class AuthorizationFragment : Fragment(R.layout.fragment_authorization) {
 
         binding.etUserLogin.addTextChangedListener { viewModelAuth.onLoginChanged() }
         binding.etUserPasswords.addTextChangedListener { viewModelAuth.onPasswordChanged() }
-        binding.etUserLogin.setText("userAlexandr")
-        binding.etUserPasswords.setText("alex123")
 
         viewLifecycleOwner.lifecycleScope.launch {
             viewModelAuth.state.collect { state ->
